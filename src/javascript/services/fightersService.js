5d1d41c3654ed79d802d1e676e50cc95accd73f6
LDFandinoR <101162427+LDFandinoR@users.noreply.github.com>
2023-06-23 00:06:13 -0500

    Update fightersService.js

--- src/javascript/services/fightersService.js
+++ src/javascript/services/fightersService.js
@@ -1,4 +1,6 @@
 import callApi from '../helpers/apiHelper';
+import { getFighterInfo } from '../components/fighterSelector';
+import { createFighterPreview } from '../components/fighterPreview'
 
 class FighterService {
     #endpoint = 'fighters.json';
@@ -6,16 +8,34 @@ class FighterService {
     async getFighters() {
         try {
             const apiResult = await callApi(this.#endpoint);
+
             return apiResult;
         } catch (error) {
             throw error;
         }
     }
 
+    
+
     async getFighterDetails(id) {
         // todo: implement this method
         // endpoint - `details/fighter/${id}.json`;
+        try {
+            const endpoint = `details/fighter/${id}.json`;
+            const apiResult = await callApi(endpoint, 'GET');
+            console.log('id '+ apiResult['_id']);
+            console.log(JSON.stringify(apiResult));
+            const fighterInf = getFighterInfo(apiResult['_id']);
+            const prev = createFighterPreview(fighterInf, 'right');
+            return prev;
+          } catch (error) {
+            throw error;
+          }
+        
     }
+
+
+      
 }
 
 const fighterService = new FighterService();
