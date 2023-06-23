a06bf6b6b86b7ab63c08f2516a9da5716a66dab3
LDFandinoR <101162427+LDFandinoR@users.noreply.github.com>
2023-06-23 00:10:50 -0500

    Update fight.js

--- src/javascript/components/fight.js
+++ src/javascript/components/fight.js
@@ -3,17 +3,28 @@ import controls from '../../constants/controls';
 export async function fight(firstFighter, secondFighter) {
     return new Promise(resolve => {
         // resolve the promise with the winner when fight is over
+        
     });
 }
 
 export function getDamage(attacker, defender) {
+    const damage = getHitPower(attacker) - getBlockPower(defender)
+    if (damage > 0) {
+        return damage
+    } else {return 0}
     // return damage
 }
 
 export function getHitPower(fighter) {
+    const criticalHitChance = getRandomFloatFromRange(1, 2);
+    const hitPower = fighter.attack * criticalHitChance;
     // return hit power
+    return hitPower
 }
 
 export function getBlockPower(fighter) {
+    const dodgeChance = getRandomFloatFromRange(1, 2);
+    const blockPower = fighter.defense * dodgeChance;
     // return block power
+    return blockPower
 }
